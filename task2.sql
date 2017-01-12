af7c37d1c7475f93c005f76e5318858907d30541
Alex Banul <albanul92@gmail.com>
2017-01-12 19:49:57 +0300

    task 2 sql queries fixes

--- task2.sql
+++ task2.sql
@@ -1,32 +1,8 @@
-﻿Sales: Id, ProductId, CustomerId, DateCreated
+﻿-- Если столбец [id] - суррогантый ключ, тогда он уникальный, 
+-- значит может быть два варианта
 
---	Если заказ харектеризуется общим [Id], а порядок элементов характеризуется полем [DateCreated]
--- 	как в примере:
---	Id			ProductId		DateCreated 				CustomerId
---	-------		---------		---------------------		----------
---	1			1				'01/01/2016 23:59:00'		1
---	1			2				'01/01/2016 23:59:10'		1
---	2			1				'02/01/2016 10:00:00'		1
---	2			2				'02/01/2016 10:00:10'		1
-
--- тогда запрос будет выглядеть таким образом:
-SELECT
-	s.[ProductId]
-	,COUNT(s.[ProductId]) AS [count],
-	s.[CustomerId]
-FROM [Sales] s
-JOIN (
-		SELECT
-			[Id]
-			,MIN([DateCreated]) AS [minDate]
-		FROM [Sales]
-		GROUP BY [Id]
-	) salesIdAndMinDate
-	ON salesIdAndMinDate.[Id] = s.[Id]
-	AND salesIdAndMinDate.[minDate] = s.[DateCreated]
-GROUP BY s.[ProductId], s.[CustomerId]
-
---	Если же заказ харектеризуется общим [DateCreated], а порядок элементов характеризуется полем [Id]
+--	Если пользователь может купить несколько продуктов одновременно в один момент времени [DateCreated] 
+--  и порядок элементов характеризуется полем [Id]
 -- 	как в примере:
 --	Id			ProductId		DateCreated 				CustomerId
 --	-------		---------		---------------------		----------
@@ -34,18 +10,50 @@ GROUP BY s.[ProductId], s.[CustomerId]
 --	2			2				'01/01/2016 23:59:00'		1
 --	3			1				'02/01/2016 10:00:10'		1
 --	4			2				'02/01/2016 10:00:10'		1
+--	5			3				'01/01/2016 23:59:00'		2
+--	6			4				'01/01/2016 23:59:00'		2
+--	7			3				'02/01/2016 10:00:10'		2
+--	8			4				'02/01/2016 10:00:10'		2
 
 -- тогда запрос будет выглядеть таким образом:
 SELECT
 	s.[ProductId]
 	,COUNT(s.[ProductId]) AS [count]
-	,s.[CustomerId]
 FROM [Sales] s
 JOIN (
 		SELECT
 			MIN([Id]) AS [minId]
 		FROM [Sales]
 		GROUP BY [DateCreated], [CustomerId]
-	) minProdId
-	ON minProdId.[minId] = s.[Id]
-GROUP BY s.[ProductId], s.[CustomerId]
+	) firstProductId
+	ON firstProductId.[minId] = s.[Id]
+GROUP BY s.[ProductId]
+
+--	Если же пользователь может купить только один продукт в один момент времени [DateCreated]
+-- 	как в примере:
+--	Id			ProductId		DateCreated 				CustomerId
+--	-------		---------		---------------------		----------
+--	1			1				'01/01/2016 23:59:00'		1
+--	2			2				'01/01/2016 23:59:01'		1
+--	3			1				'02/01/2016 10:00:10'		1
+--	4			2				'02/01/2016 10:00:11'		1
+--	5			1				'01/01/2016 23:59:02'		2
+--	6			2				'01/01/2016 23:59:03'		2
+--	7			1				'02/01/2016 10:00:12'		2
+--	8			2				'02/01/2016 10:00:13'		2
+
+-- тогда запрос будет выглядеть таким образом:
+SELECT
+	s.[ProductId]
+	,COUNT(s.[ProductId]) AS [count]
+FROM [Sales] s
+JOIN (
+		SELECT
+			MIN([DateCreated]) AS [minDate],
+			[CustomerId]
+		FROM [Sales]
+		GROUP BY [CustomerId]
+	) minTime
+	ON s.[CustomerId] = minTime.[CustomerId] 
+	AND minTime.[minDate] = s.[DateCreated]
+GROUP BY s.[ProductId]
